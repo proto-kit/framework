2a26f221196c975f354fb1d88da07b2fa0108bde
Matej Sima <matej.sima@gmail.com>
2023-05-06 11:06:58 +0200

    feat(module): updated HashList to have a default zero value

--- packages/module/src/utils/HashList.ts
+++ packages/module/src/utils/HashList.ts
@@ -1,20 +1,16 @@
+/* eslint-disable new-cap */
 /* eslint-disable import/prefer-default-export */
 /* eslint-disable @typescript-eslint/prefer-readonly-parameter-types */
-import { type Field, type FlexibleProvablePure, Poseidon } from 'snarkyjs';
+import { Field, type FlexibleProvablePure, Poseidon } from 'snarkyjs';
 
 export class HashList<Value> {
-  public static fromFields<Value>(
-    valueType: FlexibleProvablePure<Value>,
-    value: Value
-  ) {
-    const hash = Poseidon.hash(valueType.toFields(value));
-    return new HashList(valueType, hash);
+  public static fromType<Value>(valueType: FlexibleProvablePure<Value>) {
+    return new HashList(valueType);
   }
 
-  public constructor(
-    public valueType: FlexibleProvablePure<Value>,
-    public hash: Field
-  ) {}
+  public hash: Field = Field(0);
+
+  public constructor(public valueType: FlexibleProvablePure<Value>) {}
 
   public push(value: Value) {
     this.hash = Poseidon.hash([this.hash, ...this.valueType.toFields(value)]);
