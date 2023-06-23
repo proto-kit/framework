ffca56595356fc80fc4bc0d6470eba4c3b8c686e
Raphael Panic <raphael.panic@gmx.at>
2023-06-23 11:47:44 +0200

    feat: Added prepare execution in worker

--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -7,3 +7,10 @@ export type TypedClass<Class> = new (...args: any[]) => Class;
  */
 export type StringKeyOf<Target extends object> = Extract<keyof Target, string>;
 // export type StringKeyOf<Target extends object> = keyof Target
+
+/**
+ * Utility type to infer element type from an array type
+ */
+export type ArrayElement<ArrayType extends readonly unknown[]> =
+  // eslint-disable-next-line putout/putout
+  ArrayType extends readonly (infer ElementType)[] ? ElementType : never;
