89ced63f99b048ce65e6bd6218ac7433857b8587
Raphael Panic <raphael.panic@gmx.at>
2023-06-28 14:31:02 +0200

    fix: eslint file-wide disabling

--- packages/sequencer/src/worker/manager/MapReduceFlow.ts
+++ packages/sequencer/src/worker/manager/MapReduceFlow.ts
@@ -1,5 +1,6 @@
-/* eslint-disable max-len,promise/avoid-new,promise/prefer-await-to-then,promise/always-return,@typescript-eslint/no-empty-function,etc/no-implicit-any-catch,putout/putout */
+/* eslint-disable promise/avoid-new */
 import { log } from "@yab/common";
+import { noop } from "@yab/protocol";
 
 import { Closeable, InstantiatedQueue, TaskQueue } from "../queue/TaskQueue";
 
@@ -14,6 +15,8 @@ const errors = {
       "Queue not initialized, call this method only through executeFlowWithQueue()"
     ),
 };
+
+export const TASKS_REDUCE_SUFFIX = "_reduce";
 
 /**
  * Task-runner that has the responsibility of pushing and coordinating
@@ -37,7 +40,8 @@ export class MapReduceFlow<Input, Result> implements Closeable {
   /**
    * @param messageQueue The connection object to the messageQueue
    * @param mapReduceTask The task definition to be executed
-   * @param queueName The name of the queue on which the task should be executed (workers have to listen on that queue)
+   * @param queueName The name of the queue on which the task should be
+   * executed (workers have to listen on that queue)
    */
   public constructor(
     protected readonly messageQueue: TaskQueue,
@@ -59,20 +63,19 @@ export class MapReduceFlow<Input, Result> implements Closeable {
     let { pendingInputs } = this;
     const { mapReduceTask } = this;
 
-    for (let index = 0; index < pendingInputs.length; index++) {
-      const first = pendingInputs[index];
+    for (const [index, first] of pendingInputs.entries()) {
       const secondIndex = pendingInputs.findIndex(
         (second, index2) =>
           index2 > index && mapReduceTask.reducible(first, second)
       );
 
       if (secondIndex > 0) {
-        const second = pendingInputs[secondIndex];
+        const r2 = pendingInputs[secondIndex];
         pendingInputs = pendingInputs.filter(
           (unused, index2) => index2 !== index && index2 !== secondIndex
         );
 
-        res.push({ r1: first, r2: second });
+        res.push({ r1: first, r2 });
       }
     }
 
@@ -83,7 +86,7 @@ export class MapReduceFlow<Input, Result> implements Closeable {
 
   private async pushReduction(t1: Result, t2: Result) {
     const payload: TaskPayload = {
-      name: `${this.mapReduceTask.name()}_reduce`,
+      name: `${this.mapReduceTask.name()}${TASKS_REDUCE_SUFFIX}`,
 
       payload: JSON.stringify([
         this.serializer.toJSON(t1),
@@ -106,14 +109,16 @@ export class MapReduceFlow<Input, Result> implements Closeable {
       async ({ r1, r2 }) => await this.pushReduction(r1, r2)
     );
 
-    // We additionally need this count, because otherwise there would be a race condition to mark a task as completed, if we would do it via runningTasks
+    // We additionally need this count, because otherwise there would be a race
+    // condition to mark a task as completed, if we would do it via runningTasks
     this.runningTaskCount += tasks.length;
 
     await Promise.all(promises);
   }
 
   // Handles the result of a reduce operation
-  // It checks whether new reductions are available to compute or if the reduction has been completed
+  // It checks whether new reductions are available to compute or if the
+  // reduction has been completed
   protected async handleCompletedReducingStep(
     payload: TaskPayload,
     resolve: (result: Result) => void
@@ -153,10 +158,8 @@ export class MapReduceFlow<Input, Result> implements Closeable {
       this.assertQueueNotNull(queue);
 
       // Register result listener
-      await queue.onCompleted(async (result) => {
-        const { payload } = result;
-
-        if (payload.name === `${mapReduceTask.name()}_reduce`) {
+      await queue.onCompleted(async ({ payload }) => {
+        if (payload.name === `${mapReduceTask.name()}${TASKS_REDUCE_SUFFIX}`) {
           await this.handleCompletedReducingStep(payload, resolve);
           // eslint-disable-next-line sonarjs/elseif-without-else
         } else if (payload.name === mapReduceTask.name()) {
@@ -200,8 +203,10 @@ export class MapReduceFlow<Input, Result> implements Closeable {
     const promise = new Promise<Result>((resolve, reject) => {
       boundExecutor(resolve)
         // Do we need then()?
-        .then(() => {})
-        .catch((error) => {
+        // eslint-disable-next-line promise/prefer-await-to-then
+        .then(noop)
+        // eslint-disable-next-line promise/prefer-await-to-then
+        .catch((error: unknown) => {
           reject(error);
         });
     });
