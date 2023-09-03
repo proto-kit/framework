ee1979fa8f5903d9902d2786e70bc367c4f26b68
Raphael Panic <raphael.panic@gmx.at>
2023-09-03 10:46:11 +0200

    feat: Finished AccountStateModule

--- packages/protocol/src/blockmodules/AccountStateModule.ts
+++ packages/protocol/src/blockmodules/AccountStateModule.ts
@@ -1,18 +1,28 @@
-import { FlexibleProvable, PublicKey, Struct, UInt64 } from "snarkyjs";
+import { PublicKey, Struct, UInt64 } from "snarkyjs";
 
-import { BlockModule } from "../protocol/BlockModule";
 import { BlockProverExecutionData } from "../prover/block/BlockProvable";
 import { StateMap } from "../state/StateMap";
 import { protocolState } from "../state/protocol/ProtocolState";
+import { ProvableTransactionHook } from "../protocol/ProvableTransactionHook";
 
 export class AccountState extends Struct({
   nonce: UInt64,
 }) {}
 
-export class AccountStateModule implements BlockModule {
-  @protocolState() accountState = StateMap.from(PublicKey, AccountState);
+export class AccountStateModule extends ProvableTransactionHook {
+  @protocolState() public accountState = StateMap.from(PublicKey, AccountState);
 
-  public createTransitions(executionData: BlockProverExecutionData) {
-    const x: FlexibleProvable<AccountState> = AccountState;
+  public onTransaction({ transaction }: BlockProverExecutionData): void {
+    const accountState = this.accountState
+      .get(transaction.sender)
+      .orElse(new AccountState({ nonce: UInt64.zero }));
+
+    const currentNonce = accountState.nonce;
+    currentNonce.equals(transaction.nonce).assertTrue("Nonce not matching");
+
+    this.accountState.set(
+      transaction.sender,
+      new AccountState({ nonce: currentNonce.add(1) })
+    );
   }
 }
