c2ba4a0a7ee194520a7177c2d34c8432356af960
Raphael Panic <raphael.panic@gmx.at>
2023-11-22 12:16:18 +0100

    Fixed test

--- packages/common/test/types/UInt.test.ts
+++ packages/common/test/types/UInt.test.ts
@@ -1,25 +1,39 @@
+import "reflect-metadata";
 import { UInt112 } from "../../src";
+import { container } from "tsyringe";
+import { RuntimeMethodExecutionContext } from "@proto-kit/protocol";
+import { beforeEach } from "@jest/globals";
 
 describe("uint112", () => {
+  const executionContext = container.resolve(RuntimeMethodExecutionContext);
+
+  beforeEach(() => {
+    executionContext.clear();
+    executionContext.setup({} as any);
+  })
+
   it("should initialize correctly", () => {
-    expect.assertions(1);
+    expect.assertions(2);
 
     const uint = UInt112.from(1);
 
     expect(uint.value.toBigInt()).toBe(1n);
+    expect(executionContext.result.status.toBoolean()).toBe(true);
   });
 
   it("should mul correctly", () => {
-    expect.assertions(1);
+    expect.assertions(2);
 
     const uint = UInt112.from(101);
 
     expect(uint.mul(987654).toBigInt()).toBe(101n * 987654n)
+    expect(executionContext.result.status.toBoolean()).toBe(true);
   });
 
   it("should fail for init with higher value", () => {
     expect.assertions(1);
 
+    // We throw on constant inits
     expect(() => {
       UInt112.from(2n ** 128n);
     }).toThrow(/.*/u);
@@ -30,9 +44,9 @@ describe("uint112", () => {
 
     const uint = UInt112.from(2n ** 111n + 1n);
 
-    expect(() => {
-      uint.add(uint.value.toBigInt());
-    }).toThrow(/.*/u);
+    uint.add(uint.value.toBigInt());
+
+    expect(executionContext.result.status.toBoolean()).toBe(false);
   });
 
   it("should fail for overflow multiplication", () => {
@@ -40,8 +54,8 @@ describe("uint112", () => {
 
     const uint = UInt112.from(2n ** 110n);
 
-    expect(() => {
-      uint.mul(uint.value.toBigInt());
-    }).toThrow(/.*/u);
+    uint.mul(uint.value.toBigInt());
+
+    expect(executionContext.result.status.toBoolean()).toBe(false);
   });
 });
