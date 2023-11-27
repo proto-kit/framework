74cf0a2f6bf26bbc165e00641cd530894a203c6d
Raphael Panic <raphael.panic@gmx.at>
2023-11-27 11:41:42 +0100

    Added UInt32 and UInt64

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -10,6 +10,8 @@ export * from "./utils";
 export * from "./dependencyFactory/DependencyFactory";
 export * from "./log";
 export * from "./types/UInt";
+export * from "./types/UInt32";
+export * from "./types/UInt64";
 export * from "./types/UInt112";
 export * from "./types/UInt224";
 export * from "./types/PrecisionHelper";
