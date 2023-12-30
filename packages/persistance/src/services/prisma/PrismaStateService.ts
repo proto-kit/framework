0245814c601c4ccb1d1457e7cf6978e5f5411ca8
Raphael Panic <raphael.panic@gmx.at>
2023-12-30 23:37:02 +0100

    Changed datatype in stateservice from bigint to decimal

--- packages/persistance/src/services/prisma/PrismaStateService.ts
+++ packages/persistance/src/services/prisma/PrismaStateService.ts
@@ -1,29 +1,39 @@
 import { AsyncStateService } from "@proto-kit/sequencer";
 import { Field } from "o1js";
 import { PrismaDatabaseConnection } from "../../PrismaDatabaseConnection";
+import { inject, injectable } from "tsyringe";
+import { Prisma } from "@prisma/client";
+import { noop } from "@proto-kit/common";
 
+@injectable()
 export class PrismaStateService implements AsyncStateService {
   private cache: [Field, Field[] | undefined][] = [];
 
-  public constructor(private readonly connection: PrismaDatabaseConnection) {}
+  /**
+   * @param connection
+   * @param mask A indicator to which masking level the values belong
+   */
+  public constructor(
+    @inject("Database") private readonly connection: PrismaDatabaseConnection,
+    private readonly mask: string = "base"
+  ) {}
 
   public async commit(): Promise<void> {
     const { client } = this.connection;
 
     const data = this.cache
       .filter((entry) => entry[1] !== undefined)
-      .map((entry) => {
-        return {
-          path: entry[0].toBigInt(),
-          values: entry[1]!.map((field) => field.toBigInt()),
-        };
-      });
+      .map((entry) => ({
+        path: new Prisma.Decimal(entry[0].toString()),
+        values: entry[1]!.map((field) => new Prisma.Decimal(field.toString())),
+        mask: this.mask,
+      }));
 
     await client.$transaction([
       client.state.deleteMany({
         where: {
           path: {
-            in: this.cache.map((x) => x[0].toBigInt()),
+            in: this.cache.map((x) => new Prisma.Decimal(x[0].toString())),
           },
         },
       }),
@@ -32,33 +42,30 @@ export class PrismaStateService implements AsyncStateService {
       }),
     ]);
 
-    // const removeKeys = this.cache
-    //   .filter((x) => x[1] === undefined)
-    //   .map((x) => x[0]);
-    //
-    // if (removeKeys.length > 0) {
-    //   await this.client.client.state.deleteMany({
-    //     where: {
-    //       path: {
-    //         in: removeKeys.map((x) => x.toBigInt()),
-    //       },
-    //     },
-    //   });
-    // }
-
     this.cache = [];
   }
 
   public async getAsync(key: Field): Promise<Field[] | undefined> {
     const record = await this.connection.client.state.findFirst({
       where: {
-        path: key.toBigInt(),
+        AND: [
+          {
+            path: new Prisma.Decimal(key.toString()),
+          },
+          {
+            mask: this.mask,
+          },
+        ],
       },
     });
-    return record?.values.map((x) => Field(x)) ?? undefined;
+    // x.toNumber() is safe, because we know that the actual DB-type
+    // is a decimal with 0 decimal places
+    return record?.values.map((x) => Field(x.toNumber())) ?? undefined;
   }
 
-  public async openTransaction(): Promise<void> {}
+  public async openTransaction(): Promise<void> {
+    noop();
+  }
 
   public async setAsync(key: Field, value: Field[] | undefined): Promise<void> {
     this.cache.push([key, value]);
