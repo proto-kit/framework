9e670619c156503239ee8bee628eef6f3eacc005
Raphael Panic <raphael.panic@gmx.at>
2024-01-04 16:17:17 +0100

    Added configurability for Prisma Connector

--- packages/persistance/src/PrismaDatabaseConnection.ts
+++ packages/persistance/src/PrismaDatabaseConnection.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client";
 import {
   sequencerModule,
   SequencerModule,
-  StorageDependencyMinimumDependencies
+  StorageDependencyMinimumDependencies,
 } from "@proto-kit/sequencer";
 import { DependencyFactory, noop } from "@proto-kit/common";
 
@@ -10,12 +10,32 @@ import { PrismaStateService } from "./services/prisma/PrismaStateService";
 import { PrismaBatchStore } from "./services/prisma/PrismaBatchStore";
 import { PrismaBlockStorage } from "./services/prisma/PrismaBlockStorage";
 
+export interface PrismaDatabaseConfig {
+  connection?: {
+    username: string;
+    password: string;
+    host: string;
+    port?: number;
+    db?: {
+      name: string;
+      schema?: string;
+    };
+  };
+}
+
 @sequencerModule()
 export class PrismaDatabaseConnection
-  extends SequencerModule
+  extends SequencerModule<PrismaDatabaseConfig>
   implements DependencyFactory
 {
-  public readonly client = new PrismaClient();
+  private initializedClient: PrismaClient | undefined = undefined;
+
+  public get client(): PrismaClient {
+    if (this.initializedClient === undefined) {
+      throw new Error("Client not initialized yet, wait for after the startup");
+    }
+    return this.initializedClient;
+  }
 
   public dependencies(): Omit<
     StorageDependencyMinimumDependencies,
@@ -41,6 +61,29 @@ export class PrismaDatabaseConnection
   }
 
   public async start(): Promise<void> {
-    noop();
+    const { connection } = this.config;
+    if (connection !== undefined) {
+      const { host, port, username, password, db } = connection;
+
+      const dbString =
+        db !== undefined
+          ? `${db.name}?schema=${db.schema ?? "public"}`
+          : "protokit?schema=public";
+
+      const url = `postgresql://${username}:${password}@${host}:${
+        // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+        port ?? 5432
+      }/${dbString}`;
+
+      this.initializedClient = new PrismaClient({
+        datasources: {
+          db: {
+            url,
+          },
+        },
+      });
+    } else {
+      this.initializedClient = new PrismaClient();
+    }
   }
 }
