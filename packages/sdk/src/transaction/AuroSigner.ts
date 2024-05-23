49b13d8f5aa8f2eee375cb8c54ea46f2f46b2076
asimaranov <a.simaranov@gmail.com>
2024-05-23 17:05:30 +0300

    Fixes for shim connect

--- packages/sdk/src/transaction/AuroSigner.ts
+++ packages/sdk/src/transaction/AuroSigner.ts
@@ -7,9 +7,16 @@ import { Signer } from "./InMemorySigner";
 @injectable()
 export class AuroSigner extends AppChainModule<unknown> implements Signer {
   public async sign(message: Field[]): Promise<Signature> {
+    try {
     const response = await (window as any).mina.signFields({
       message: message.map((field) => field.toString()),
     });
     return Signature.fromBase58(response.signature);
-  }
+  } catch (e: any) {
+    if (e.code == 1001) {
+      await (window as any).mina.requestAccounts();
+      return await this.sign(message);
+    }
+    throw e;
+  }}
 }
