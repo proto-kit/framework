17614d4277d5115e206bd09d3ced40afa649c399
Raphael Panic <raphael.panic@gmx.at>
2024-06-24 20:36:22 +0200

    docker-compose files

--- packages/deployment/docker/base/entry.sh
+++ packages/deployment/docker/base/entry.sh
@@ -4,4 +4,5 @@ if [[ ${DATABASE_URL} ]]; then
   echo "Migrating on ${DATABASE_URL}"
   npm run migrate;
 fi
+#LOGGING-LEVEL=${LOGGING_LEVEL}
 node --experimental-vm-modules --experimental-wasm-modules --experimental-wasm-threads --es-module-specifier-resolution=node $@
