39fe6f5424426a0afde860e8f1b0b0a671bbed11
Raphael Panic <raphael.panic@gmx.at>
2024-07-16 18:23:37 +0200

    Revert "Unstable: try ts-node"
    This reverts commit 65ee542a2b4c96b16190fe98d124b1bd690fd94a.

--- packages/cli/src/cli-ts.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env ts-node-esm
-// #!/usr/bin/env node  --experimental-vm-modules --experimental-wasm-modules --experimental-wasm-threads
-
-import "cli";
-
-console.log("Start ts");
--- packages/cli/src/cli.ts
+++ packages/cli/src/cli.ts
@@ -41,5 +41,3 @@ yargs(hideBin(process.argv))
     start
   )
   .help().argv;
-
-export {};
