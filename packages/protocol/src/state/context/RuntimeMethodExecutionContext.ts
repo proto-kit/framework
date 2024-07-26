088de136ead8c65ee6de30c437ff736cda38a18b
ejMina226 <118474890+ejMina226@users.noreply.github.com>
2024-07-26 14:53:10 +0100

    Add in addEvent to RuntimeMethodExecutionContext

--- packages/protocol/src/state/context/RuntimeMethodExecutionContext.ts
+++ packages/protocol/src/state/context/RuntimeMethodExecutionContext.ts
@@ -1,4 +1,4 @@
-import { Bool, Provable, Struct } from "o1js";
+import { Bool, FlexibleProvablePure, Provable, Struct } from "o1js";
 import { singleton } from "tsyringe";
 import {
   ProvableMethodExecutionContext,
@@ -24,6 +24,8 @@ export class RuntimeProvableMethodExecutionResult extends ProvableMethodExecutio
   public statusMessage?: string;
 
   public stackTrace?: string;
+
+  public events: any[] = [];
 }
 
 export interface RuntimeMethodExecutionData {
@@ -71,6 +73,11 @@ export class RuntimeMethodExecutionContext extends ProvableMethodExecutionContex
   public addStateTransition<Value>(stateTransition: StateTransition<Value>) {
     this.assertSetupCalled();
     this.result.stateTransitions.push(stateTransition);
+  }
+
+  public addEvent(event: FlexibleProvablePure<any>[]) {
+    this.assertSetupCalled();
+    this.result.events.push(event);
   }
 
   /**
