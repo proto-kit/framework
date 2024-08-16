ad0cde0725c2e96181d63ee14a57b43104179661
Raphael Panic <raphael.panic@gmx.at>
2024-08-16 15:11:14 +0200

    Added FeeStrategies

---
+++ packages/sequencer/src/protocol/baselayer/fees/ConstantFeeStrategy.ts
@@ -0,0 +1,26 @@
+import {
+  sequencerModule,
+  SequencerModule,
+} from "../../../sequencer/builder/SequencerModule";
+import { noop } from "@proto-kit/common";
+import { FeeStrategy } from "./FeeStrategy";
+
+export type ConstantFeeStrategyConfig = {
+  fee?: number;
+};
+
+const DEFAULT_FEE = 0.1 * 1e9;
+
+@sequencerModule()
+export class ConstantFeeStrategy
+  extends SequencerModule<ConstantFeeStrategyConfig>
+  implements FeeStrategy
+{
+  getFee(): number {
+    return this.config.fee ?? DEFAULT_FEE;
+  }
+
+  public async start() {
+    noop();
+  }
+}
---
+++ packages/sequencer/src/protocol/baselayer/fees/FeeStrategy.ts
@@ -0,0 +1,3 @@
+export interface FeeStrategy {
+  getFee(): number;
+}
