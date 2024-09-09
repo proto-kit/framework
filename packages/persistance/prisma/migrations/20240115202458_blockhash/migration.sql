82bd226ddab2c54cd26a2df1c50af88540d1739e
Matej Sima <matej.sima@gmail.com>
2024-09-09 13:51:48 +0200

    bring back original migrations

--- packages/persistance/prisma/migrations/20240115202458_blockhash/migration.sql
+++ packages/persistance/prisma/migrations/20240115202458_blockhash/migration.sql
@@ -1,10 +1,24 @@
--- CreateTable
-CREATE TABLE "State" (
-    "path" DECIMAL(78,0) NOT NULL,
-    "values" DECIMAL(78,0)[],
-    "mask" VARCHAR(256) NOT NULL,
+/*
+  Warnings:
 
-    CONSTRAINT "State_pkey" PRIMARY KEY ("path","mask")
+  - The primary key for the `State` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - Added the required column `mask` to the `State` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "State" DROP CONSTRAINT "State_pkey",
+ADD COLUMN     "mask" VARCHAR(256) NOT NULL,
+ALTER COLUMN "path" SET DATA TYPE DECIMAL(78,0),
+ALTER COLUMN "values" SET DATA TYPE DECIMAL(78,0)[],
+ADD CONSTRAINT "State_pkey" PRIMARY KEY ("path", "mask");
+
+-- CreateTable
+CREATE TABLE "TreeElement" (
+    "key" DECIMAL(78,0) NOT NULL,
+    "level" SMALLINT NOT NULL,
+    "value" DECIMAL(78,0) NOT NULL,
+
+    CONSTRAINT "TreeElement_pkey" PRIMARY KEY ("key","level")
 );
 
 -- CreateTable
@@ -13,11 +27,9 @@ CREATE TABLE "Transaction" (
     "methodId" TEXT NOT NULL,
     "sender" TEXT NOT NULL,
     "nonce" TEXT NOT NULL,
-    "argsFields" TEXT[],
-    "auxiliaryData" TEXT[],
+    "args" TEXT[],
     "signature_r" TEXT NOT NULL,
     "signature_s" TEXT NOT NULL,
-    "isMessage" BOOLEAN NOT NULL,
 
     CONSTRAINT "Transaction_pkey" PRIMARY KEY ("hash")
 );
@@ -44,8 +56,6 @@ CREATE TABLE "Block" (
     "fromEternalTransactionsHash" TEXT NOT NULL,
     "toEternalTransactionsHash" TEXT NOT NULL,
     "fromBlockHashRoot" TEXT NOT NULL,
-    "fromMessagesHash" TEXT NOT NULL,
-    "toMessagesHash" TEXT NOT NULL,
     "parentHash" TEXT,
     "batchHeight" INTEGER,
 
@@ -56,13 +66,12 @@ CREATE TABLE "Block" (
 CREATE TABLE "Batch" (
     "height" INTEGER NOT NULL,
     "proof" JSON NOT NULL,
-    "settlementTransactionHash" TEXT,
 
     CONSTRAINT "Batch_pkey" PRIMARY KEY ("height")
 );
 
 -- CreateTable
-CREATE TABLE "BlockResult" (
+CREATE TABLE "UnprovenBlockMetadata" (
     "blockHash" TEXT NOT NULL,
     "stateRoot" TEXT NOT NULL,
     "blockHashRoot" TEXT NOT NULL,
@@ -70,39 +79,14 @@ CREATE TABLE "BlockResult" (
     "blockStateTransitions" JSON NOT NULL,
     "blockHashWitness" JSON NOT NULL,
 
-    CONSTRAINT "BlockResult_pkey" PRIMARY KEY ("blockHash")
-);
-
--- CreateTable
-CREATE TABLE "Settlement" (
-    "transactionHash" TEXT NOT NULL,
-    "promisedMessagesHash" TEXT NOT NULL,
-
-    CONSTRAINT "Settlement_pkey" PRIMARY KEY ("transactionHash")
-);
-
--- CreateTable
-CREATE TABLE "IncomingMessageBatchTransaction" (
-    "transactionHash" TEXT NOT NULL,
-    "batchId" INTEGER NOT NULL,
-
-    CONSTRAINT "IncomingMessageBatchTransaction_pkey" PRIMARY KEY ("transactionHash","batchId")
-);
-
--- CreateTable
-CREATE TABLE "IncomingMessageBatch" (
-    "id" SERIAL NOT NULL,
-    "fromMessageHash" TEXT NOT NULL,
-    "toMessageHash" TEXT NOT NULL,
-
-    CONSTRAINT "IncomingMessageBatch_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "UnprovenBlockMetadata_pkey" PRIMARY KEY ("blockHash")
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Block_parentHash_key" ON "Block"("parentHash");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "BlockResult_blockHash_key" ON "BlockResult"("blockHash");
+CREATE UNIQUE INDEX "UnprovenBlockMetadata_blockHash_key" ON "UnprovenBlockMetadata"("blockHash");
 
 -- AddForeignKey
 ALTER TABLE "TransactionExecutionResult" ADD CONSTRAINT "TransactionExecutionResult_txHash_fkey" FOREIGN KEY ("txHash") REFERENCES "Transaction"("hash") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -117,13 +101,4 @@ ALTER TABLE "Block" ADD CONSTRAINT "Block_parentHash_fkey" FOREIGN KEY ("parentH
 ALTER TABLE "Block" ADD CONSTRAINT "Block_batchHeight_fkey" FOREIGN KEY ("batchHeight") REFERENCES "Batch"("height") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Batch" ADD CONSTRAINT "Batch_settlementTransactionHash_fkey" FOREIGN KEY ("settlementTransactionHash") REFERENCES "Settlement"("transactionHash") ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "BlockResult" ADD CONSTRAINT "BlockResult_blockHash_fkey" FOREIGN KEY ("blockHash") REFERENCES "Block"("hash") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "IncomingMessageBatchTransaction" ADD CONSTRAINT "IncomingMessageBatchTransaction_transactionHash_fkey" FOREIGN KEY ("transactionHash") REFERENCES "Transaction"("hash") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "IncomingMessageBatchTransaction" ADD CONSTRAINT "IncomingMessageBatchTransaction_batchId_fkey" FOREIGN KEY ("batchId") REFERENCES "IncomingMessageBatch"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "UnprovenBlockMetadata" ADD CONSTRAINT "UnprovenBlockMetadata_blockHash_fkey" FOREIGN KEY ("blockHash") REFERENCES "Block"("hash") ON DELETE RESTRICT ON UPDATE CASCADE;
