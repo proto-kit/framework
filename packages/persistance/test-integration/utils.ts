55d8d340eccc67498fadafbc0afa8851325a0f29
ejMina226 <118474890+ejMina226@users.noreply.github.com>
2024-10-02 16:18:43 +0100

    Add in missing references

--- packages/persistance/test-integration/utils.ts
+++ packages/persistance/test-integration/utils.ts
@@ -151,6 +151,7 @@ export function createPrismaAppchain(
         BlockReductionTask: {},
         BlockProvingTask: {},
         BlockBuildingTask: {},
+        CircuitCompilerTask: {},
       },
       BaseLayer: {},
       BlockProducerModule: {},
