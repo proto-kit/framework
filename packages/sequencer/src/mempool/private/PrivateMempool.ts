b916e582082bb3616344a9e14b07ccbcbb544ee1
ejMina226 <118474890+ejMina226@users.noreply.github.com>
2024-10-04 16:03:46 +0100

    Chnage test and start more complex implementation

--- packages/sequencer/src/mempool/private/PrivateMempool.ts
+++ packages/sequencer/src/mempool/private/PrivateMempool.ts
@@ -1,5 +1,11 @@
 import { EventEmitter, log, noop } from "@proto-kit/common";
 import { inject } from "tsyringe";
+import {
+  AccountStateHook,
+  MandatoryProtocolModulesRecord,
+  Protocol,
+  ProvableTransactionHook,
+} from "@proto-kit/protocol";
 
 import type { Mempool, MempoolEvents } from "../Mempool";
 import type { PendingTransaction } from "../PendingTransaction";
@@ -14,12 +20,18 @@ import { TransactionValidator } from "../verification/TransactionValidator";
 export class PrivateMempool extends SequencerModule implements Mempool {
   public readonly events = new EventEmitter<MempoolEvents>();
 
+  private readonly accountStateHook: AccountStateHook;
+
   public constructor(
     private readonly transactionValidator: TransactionValidator,
     @inject("TransactionStorage")
-    private readonly transactionStorage: TransactionStorage
+    private readonly transactionStorage: TransactionStorage,
+    @inject("Protocol")
+    private readonly protocol: Protocol<MandatoryProtocolModulesRecord>
   ) {
     super();
+    this.accountStateHook =
+      this.protocol.dependencyContainer.resolve("AccountStateHook");
   }
 
   public async add(tx: PendingTransaction): Promise<boolean> {
@@ -50,17 +62,19 @@ export class PrivateMempool extends SequencerModule implements Mempool {
     );
   }
 
-  compareTxs(x: PendingTransaction, y: PendingTransaction): number {
-    if (x.sender === y.sender) {
-      return x.nonce.toBigInt() - y.nonce.toBigInt() > 0 ? 1 : -1;
-    }
-    return x.sender.toBase58().localeCompare(y.sender.toBase58());
-  }
-
   public async getTxs(): Promise<PendingTransaction[]> {
-    return (await this.transactionStorage.getPendingUserTransactions()).sort(
-      this.compareTxs
-    );
+    const txs = await this.transactionStorage.getPendingUserTransactions();
+    const sortedTxs: PendingTransaction[] = [];
+    for (const tx of txs) {
+      const signedTransaction = tx.toProtocolTransaction();
+      const txWithHooks = this.accountStateHook.onTransaction({
+        networkState: this.protocol.n,
+        transaction: signedTransaction.transaction,
+        signature: signedTransaction.signature,
+      });
+      sortedTxs.push(tx);
+    }
+    return sortedTxs;
   }
 
   public async start(): Promise<void> {
