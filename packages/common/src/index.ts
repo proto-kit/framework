5d5383aa89f608d75cbb3402879b7db9eb040c3f
ejMina226 <118474890+ejMina226@users.noreply.github.com>
2024-12-20 08:10:19 +0000

    Add back in ProvableMethodExecutionContext

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -14,3 +14,4 @@ export * from "./trees/InMemoryMerkleTreeStorage";
 export * from "./trees/RollupMerkleTree";
 export * from "./events/EventEmitterProxy";
 export * from "./trees/MockAsyncMerkleStore";
+export * from "./ProvableMethodExecutionContext";
