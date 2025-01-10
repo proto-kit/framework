8bb6b68d8cd3f8a6ceaa8d3d7575b259e2f6e085
João Santos Reis <joaosreis@outlook.pt>
2025-01-10 10:48:00 +0000

    Add comments and docs to OperationQueue

--- packages/common/src/operationQueue.ts
+++ packages/common/src/operationQueue.ts
@@ -4,11 +4,22 @@ export class OperationQueue {
   private queue: Promise<void>;
 
   constructor() {
+    // Start with a resolved promise
     this.queue = Promise.resolve();
   }
 
+  /**
+   * Queue an operation to be executed
+   * @param operation - The operation to queue
+   * @returns A promise that resolves when the operation is completed
+   */
   public queueOperation<T>(operation: AsyncOperation<T>): Promise<T> {
+    // Chain the operation to the end of the queue and store the result
+    // of the queued operation to return to the caller if he wants to wait
+    // for it
     const result = this.queue.then(() => operation());
+
+    // Update the queue and discard any errors
     this.queue = result.then(
       () => undefined,
       () => undefined
@@ -16,6 +27,10 @@ export class OperationQueue {
     return result;
   }
 
+  /**
+   * Wait for all operations to complete
+   * @returns A promise that resolves when all operations are completed
+   */
   public async onCompleted(): Promise<void> {
     await this.queue;
   }
