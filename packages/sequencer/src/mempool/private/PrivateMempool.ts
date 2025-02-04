3cf6f70c23a75910d1c4a85f0ad6e9f867cd5a59
ejMina226 <118474890+ejMina226@users.noreply.github.com>
2025-02-04 12:27:25 +0000

    Add in optional flag

--- packages/sequencer/src/mempool/private/PrivateMempool.ts
+++ packages/sequencer/src/mempool/private/PrivateMempool.ts
@@ -32,8 +32,14 @@ type MempoolTransactionPaths = {
   transaction: PendingTransaction;
   paths: Field[];
 };
+interface PrivateMempoolConfig {
+  validationEnabled?: boolean;
+}
 @sequencerModule()
-export class PrivateMempool extends SequencerModule implements Mempool {
+export class PrivateMempool
+  extends SequencerModule<PrivateMempoolConfig>
+  implements Mempool
+{
   public readonly events = new EventEmitter<MempoolEvents>();
 
   private readonly accountStateHook: AccountStateHook;
@@ -100,14 +106,16 @@ export class PrivateMempool extends SequencerModule implements Mempool {
 
     const networkState =
       (await this.getStagedNetworkState()) ?? NetworkState.empty();
-
-    const sortedTxs = await this.checkTxValid(
-      txs,
-      baseCachedStateService,
-      this.protocol.stateServiceProvider,
-      networkState,
-      limit
-    );
+    const validationEnabled = this.config.validationEnabled ?? true;
+    const sortedTxs = validationEnabled
+      ? await this.checkTxValid(
+          txs,
+          baseCachedStateService,
+          this.protocol.stateServiceProvider,
+          networkState,
+          limit
+        )
+      : txs;
     this.protocol.stateServiceProvider.popCurrentStateService();
     return sortedTxs;
   }
