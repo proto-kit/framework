7ead8cc3263bfd5fb96cb95048c8230b31c42442
Raphael Panic <raphael.panic@gmx.at>
2025-02-27 17:45:57 +0100

    Added test to test mask recovery

--- packages/persistance/test-integration/SequencerRestart.test.ts
+++ packages/persistance/test-integration/SequencerRestart.test.ts
@@ -1,6 +1,6 @@
 import "reflect-metadata";
 import { afterAll, beforeAll, expect } from "@jest/globals";
-import { expectDefined } from "@proto-kit/common";
+import { expectDefined, log } from "@proto-kit/common";
 import { PrivateKey } from "o1js";
 import { container } from "tsyringe";
 
@@ -39,55 +39,110 @@ describe("sequencer restart", () => {
     await appChain.sequencer.resolve("Database").close();
   };
 
-  beforeAll(async () => {
-    await setup();
-    await clearDB();
-
+  const produce = async (num: number, type: "block" | "batch") => {
     const blockTrigger = appChain.sequencer.resolve("BlockTrigger");
 
-    console.log(sender.toPublicKey().toBase58());
-
-    for (let block = 0; block < 2; block++) {
+    for (let block = 0; block < num; block++) {
       await prepareBlock(appChain, sender.toPublicKey(), senderNonce);
       senderNonce++;
 
-      const [producedBlock] = await blockTrigger.produceBlockAndBatch();
+      const producedBlock = await blockTrigger.produceBlock();
       if ((producedBlock?.transactions.length ?? 0) === 0) {
         throw new Error(`Block not produced correctly: ${block}`);
       }
+      if (type === "batch") {
+        await blockTrigger.produceBatch();
+      }
     }
+  };
 
-    await teardown();
+  describe("resume at latest block", () => {
+    beforeAll(async () => {
+      await setup();
+      await clearDB();
 
-    await setup();
-  }, 40000);
+      await produce(2, "batch");
 
-  afterAll(async () => {
-    await teardown();
+      await teardown();
+
+      await setup();
+    }, 40000);
+
+    afterAll(async () => {
+      await teardown();
+    });
+
+    it("should fetch correct nonce", async () => {
+      const accountState =
+        await appChain.query.protocol.AccountState.accountState.get(
+          sender.toPublicKey()
+        );
+
+      expectDefined(accountState);
+
+      expect(accountState.nonce.toString()).toBe("2");
+    });
+
+    it("should be able to produce a block on top", async () => {
+      const blockTrigger = appChain.sequencer.resolve("BlockTrigger");
+      await prepareBlock(appChain, sender.toPublicKey(), senderNonce);
+      senderNonce++;
+
+      const [block, batch] = await blockTrigger.produceBlockAndBatch();
+
+      expectDefined(block);
+      expectDefined(batch);
+
+      expect(block.transactions).toHaveLength(1);
+      expect(block.transactions[0].tx.nonce.toString()).toBe("2");
+    }, 15000);
   });
 
-  it("should fetch correct nonce", async () => {
-    const accountState =
-      await appChain.query.protocol.AccountState.accountState.get(
-        sender.toPublicKey()
-      );
+  describe("reconstruct untraced block masks", () => {
+    beforeEach(async () => {
+      log.setLevel("DEBUG");
 
-    expectDefined(accountState);
+      await setup();
+      await clearDB();
 
-    expect(accountState.nonce.toString()).toBe("2");
+      await produce(2, "block");
+
+      await teardown();
+
+      await setup();
+
+      console.log("beforeEach");
+    }, 40000);
+
+    afterEach(async () => {
+      await teardown();
+    });
+
+    it("should be able to produce a block on top", async () => {
+      const blockTrigger = appChain.sequencer.resolve("BlockTrigger");
+      await prepareBlock(appChain, sender.toPublicKey(), senderNonce);
+      senderNonce++;
+
+      const block = await blockTrigger.produceBlock();
+
+      expectDefined(block);
+
+      expect(block.transactions).toHaveLength(1);
+      expect(block.transactions[0].tx.nonce.toString()).toBe("2");
+
+      // Should be able to trace after additional block
+      const batch = await blockTrigger.produceBatch();
+
+      expectDefined(batch);
+      expect(batch.blockHashes).toHaveLength(3);
+    }, 15000);
+
+    it("should be able to trace", async () => {
+      const blockTrigger = appChain.sequencer.resolve("BlockTrigger");
+      const batch = await blockTrigger.produceBatch();
+
+      expectDefined(batch);
+      expect(batch.blockHashes).toHaveLength(2);
+    });
   });
-
-  it("should be able to produce a block on top", async () => {
-    const blockTrigger = appChain.sequencer.resolve("BlockTrigger");
-    await prepareBlock(appChain, sender.toPublicKey(), senderNonce);
-    senderNonce++;
-
-    const [block, batch] = await blockTrigger.produceBlockAndBatch();
-
-    expectDefined(block);
-    expectDefined(batch);
-
-    expect(block.transactions).toHaveLength(1);
-    expect(block.transactions[0].tx.nonce.toString()).toBe("2");
-  }, 15000);
 });
