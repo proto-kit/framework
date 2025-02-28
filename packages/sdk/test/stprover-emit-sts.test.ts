b98e18621774c97b57c4346cd40ff92f2026f6c3
ejMina226 <118474890+ejMina226@users.noreply.github.com>
2025-02-28 13:47:32 +0000

    Add in tests.

---
+++ packages/sdk/test/stprover-emit-sts.test.ts
@@ -0,0 +1,77 @@
+import "reflect-metadata";
+
+import { UInt64 } from "@proto-kit/library";
+import { runtimeMethod, runtimeModule, RuntimeModule } from "@proto-kit/module";
+import { PrivateKey, Provable } from "o1js";
+import { State, state } from "@proto-kit/protocol";
+
+import { TestingAppChain } from "../src";
+
+@runtimeModule()
+class StateTester extends RuntimeModule<unknown> {
+  @state() public state1 = State.from<UInt64>(UInt64);
+
+  @runtimeMethod()
+  public async setFail() {
+    Provable.asProver(async () => {
+      await this.state1.set(UInt64.from(10));
+    });
+  }
+
+  @runtimeMethod()
+  public async setPass() {
+    await this.state1.set(UInt64.from(10));
+  }
+}
+
+describe("StateTransition", () => {
+  const senderKey = PrivateKey.random();
+
+  const appChain = TestingAppChain.fromRuntime({
+    StateTester,
+  });
+
+  beforeAll(async () => {
+    appChain.configurePartial({
+      Runtime: {
+        StateTester: {},
+        Balances: {},
+      },
+
+      Protocol: {
+        ...appChain.config.Protocol!,
+      },
+    });
+
+    await appChain.start();
+    appChain.setSigner(senderKey);
+  });
+
+  it("should fails outside provable code", async () => {
+    const stateTester = appChain.runtime.resolve("StateTester");
+    const tx1 = await appChain.transaction(
+      senderKey.toPublicKey(),
+      async () => {
+        await stateTester.setPass();
+      }
+    );
+
+    await tx1.sign();
+    await tx1.send();
+
+    await appChain.produceBlock();
+    const tx2 = await appChain.transaction(
+      senderKey.toPublicKey(),
+      async () => {
+        await stateTester.setFail();
+      }
+    );
+
+    await tx2.sign();
+    await tx2.send();
+
+    await expect(() => appChain.produceBlock()).rejects.toThrow(
+      new Error("Cannot set state inside of provable block.")
+    );
+  });
+});
