3340300ba1ed2f5a6ea8d4a2c183ec5c67bce74b
ejMina226 <118474890+ejMina226@users.noreply.github.com>
2025-03-07 16:09:49 +0000

    Add in aggregation code.

--- packages/sequencer/src/logging/ConsoleTracer.ts
+++ packages/sequencer/src/logging/ConsoleTracer.ts
@@ -1,33 +1,54 @@
-import { injectable, singleton } from "tsyringe";
+import { injectable, singleton, container } from "tsyringe";
 import { log } from "@proto-kit/common";
 
 import { Tracer } from "./Tracer";
 
-type StoreType = {
-  methodName: string;
-  invocations: { startTime: number; duration: number }[];
-};
+type StoreType = Record<string, { startTime: number; duration: number }[]>;
 
 @singleton()
 export class TraceRecordHolder {
-  public store?: StoreType;
+  public store: StoreType = {};
 }
 
 @injectable()
 export class ConsoleTracer implements Tracer {
-  // Need some change here to aggregate the same calls.
+  // Hard-code this for the moment. Needs to be configured.
+  timeInterval: number = 8000;
+
   public async trace<T>(
     name: string,
     f: () => Promise<T>,
     metadata?: Record<string, string | number | boolean>
   ): Promise<T> {
-    const timeStart = Date.now();
+    const store = container.resolve(TraceRecordHolder);
+    const methodsAlreadyLogged = Object.keys(store.store);
+
+    methodsAlreadyLogged.forEach((methodName) => {
+      if (
+        Date.now() - store.store[methodName][0].startTime >
+        this.timeInterval
+      ) {
+        const sumTime = store.store[methodName].reduce((result, curr) => {
+          return result + curr.duration;
+        }, 0);
+        const numberOfCalls = store.store[methodName].length;
+        const message = `Routine ${name}: executed ${numberOfCalls} times, average ${sumTime / numberOfCalls}ms`;
+        if (metadata !== undefined) {
+          log.debug(message, metadata);
+        } else {
+          log.debug(message);
+        }
+        delete store.store[methodName];
+      }
+    });
+
+    const startTime = Date.now();
     const result = await f();
-    const message = `Routine ${name} took ${Date.now() - timeStart}ms`;
-    if (metadata !== undefined) {
-      log.debug(message, metadata);
+    const duration = Date.now() - startTime;
+    if (name in store.store) {
+      store.store[name].push({ startTime, duration });
     } else {
-      log.debug(message);
+      store.store[name] = [{ startTime, duration }];
     }
     return result;
   }
