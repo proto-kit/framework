96f483dd2acd847c79638f061fa765dfb11a815d
ejMina226 <118474890+ejMina226@users.noreply.github.com>
2025-03-11 15:08:28 +0000

    Add in test

---
+++ packages/sequencer/test/integration/MempoolTxRemoved.test.ts
@@ -0,0 +1,74 @@
+import { Balances } from "@proto-kit/library";
+import { Runtime } from "@proto-kit/module";
+import { TestingAppChain } from "@proto-kit/sdk";
+import { Bool, PrivateKey, UInt64 } from "o1js";
+import "reflect-metadata";
+
+import { PrivateMempool, Sequencer } from "../../src";
+
+import { createTransaction } from "./utils";
+import { Balance } from "./mocks/Balance";
+
+describe("block production", () => {
+  const senderKey = PrivateKey.random();
+  const appChain = TestingAppChain.fromRuntime({ Balance });
+  let mempool: PrivateMempool;
+  let runtime: Runtime<{ Balances: typeof Balances; Balance: typeof Balance }>;
+  let sequencer: Sequencer<any>;
+
+  beforeAll(async () => {
+    appChain.configurePartial({
+      Runtime: {
+        Balances: {},
+        Balance: {},
+      },
+      Protocol: {
+        ...appChain.config.Protocol!,
+      },
+    });
+
+    await appChain.start();
+    runtime = appChain.runtime;
+    sequencer = appChain.sequencer;
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    mempool = sequencer.resolve("Mempool");
+  });
+
+  it("check tx is removed", async () => {
+    await mempool.add(
+      createTransaction({
+        runtime,
+        method: ["Balance", "setBalanceIf"],
+        privateKey: senderKey,
+        args: [senderKey.toPublicKey(), UInt64.from(100), Bool(true)],
+        nonce: 0,
+      })
+    );
+
+    await mempool.add(
+      createTransaction({
+        runtime,
+        method: ["Balance", "setBalanceIf"],
+        privateKey: senderKey,
+        args: [senderKey.toPublicKey(), UInt64.from(100), Bool(true)],
+        nonce: 1,
+      })
+    );
+
+    await appChain.produceBlock();
+
+    await mempool.add(
+      createTransaction({
+        runtime,
+        method: ["Balance", "setBalanceIf"],
+        privateKey: senderKey,
+        args: [senderKey.toPublicKey(), UInt64.from(100), Bool(true)],
+        nonce: 0,
+      })
+    );
+
+    const txs = await mempool.getTxs();
+    expect(txs.length).toBe(0);
+  }, 60_000);
+});
