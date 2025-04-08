ba261291a84edd3e22ffa669c92c8d6cd8b4d73e
Raphael Panic <raphael.panic@gmx.at>
2025-04-08 13:15:44 +0200

    Implemented circuit ops for new LMT impl

---
+++ packages/common/src/trees/lmt/LinkedMerkleTreeCircuitOps.ts
@@ -0,0 +1,229 @@
+import { Bool, Field, Provable, Struct } from "o1js";
+
+import {
+  LinkedLeafStruct,
+  LinkedMerkleTreeGlobalState,
+  LinkedMerkleTreeWitness,
+} from "./LinkedMerkleTree";
+
+/* eslint-disable no-inner-declarations */
+// TODO
+export class MonadBool extends Struct({
+  b: Bool,
+  metadata: String,
+}) {
+  and(bool2: Bool, msg: string): MonadBool {
+    const result = this.b.and(bool2);
+    let metadata: string = this.metadata;
+    Provable.asProver(() => {
+      if (!bool2.toBoolean()) {
+        metadata = metadata + (metadata.length > 0 ? "\n" : "") + msg;
+      }
+    });
+    return new MonadBool({
+      b: result,
+      metadata,
+    });
+  }
+
+  static from(b: Bool) {
+    return new MonadBool({ b, metadata: "" });
+  }
+}
+
+export type TreeWrite = {
+  path: Field;
+  from: Field;
+  to: Field;
+};
+
+export namespace LinkedMerkleTreeCircuitOps {
+  export class LinkedMerkleTreeGlobalStateWithoutRoot
+    extends Struct({
+      lastOccupiedIndex: Field,
+    })
+    implements Omit<LinkedMerkleTreeGlobalState, "root"> {}
+
+  function boolAllTrue(...args: Bool[]): Bool {
+    return args.reduce((a, b, i) => {
+      // if (!b.toBoolean()) {
+      //   console.log();
+      // }
+      return a.and(b);
+    });
+  }
+
+  export class ComputeRootInstruction extends Struct({
+    newPreviousLeaf: LinkedLeafStruct,
+    newCurrentLeaf: LinkedLeafStruct,
+    allChecksMet: Bool,
+    update: LinkedMerkleTreeGlobalStateWithoutRoot,
+  }) {}
+
+  function chooseInstruction(
+    isUpdate: Bool,
+    updateInstruction: ComputeRootInstruction,
+    insertInstruction: ComputeRootInstruction
+  ): ComputeRootInstruction {
+    return Provable.if(
+      isUpdate,
+      ComputeRootInstruction,
+      updateInstruction,
+      insertInstruction
+    );
+  }
+
+  /**
+   * verifyWitness(current.merkleWitness, current.leaf)
+   *
+   * st.path == current.leaf.path
+   * st.from.value == current.leaf.value
+   * ```
+   *
+   * updates:
+   * ```
+   * current := { current.path, current.nextPath, value: st.to.value }
+   */
+  function update(
+    state: LinkedMerkleTreeGlobalState,
+    { leafCurrent, leafPrevious }: LinkedMerkleTreeWitness,
+    { to, from, path }: TreeWrite
+  ): ComputeRootInstruction {
+    const allChecksMet = boolAllTrue(
+      path.equals(leafCurrent.leaf.path),
+      leafCurrent.leaf.value.equals(from)
+    );
+
+    return {
+      newPreviousLeaf: leafPrevious.leaf,
+      newCurrentLeaf: new LinkedLeafStruct({
+        ...leafCurrent.leaf,
+        value: to,
+      }),
+      allChecksMet,
+      update: {
+        lastOccupiedIndex: state.lastOccupiedIndex,
+      },
+    };
+  }
+
+  /**
+   * st.path == current.leaf.path
+   *
+   * previous.leaf.nextPath > current.leaf.path
+   * previous.leaf.path < current.leaf.path
+   * previous.leaf.nextPath == current.leaf.nextPath
+   *
+   * index(current.merkleWitness) == nextFreeIndex
+   *
+   * updates:
+   * previous := { previous.path, previous.value, nextPath: current.path }
+   * current := current.leaf
+   */
+  function insert(
+    state: LinkedMerkleTreeGlobalState,
+    witness: LinkedMerkleTreeWitness,
+    { path, to }: TreeWrite
+  ): ComputeRootInstruction {
+    const { leafPrevious: previous, leafCurrent: current } = witness;
+
+    const nextFreeIndex = state.lastOccupiedIndex.add(1);
+
+    const allChecksMet = boolAllTrue(
+      // Already covered in general checks
+      // path.equals(current.leaf.path),
+      current.leaf.isDummy(),
+      previous.leaf.nextPath.greaterThan(path),
+      previous.leaf.path.lessThan(path),
+      current.merkleWitness.calculateIndex().equals(nextFreeIndex)
+    );
+
+    return {
+      newPreviousLeaf: new LinkedLeafStruct({
+        ...previous.leaf,
+        nextPath: path,
+      }),
+      newCurrentLeaf: new LinkedLeafStruct({
+        path,
+        value: to,
+        nextPath: previous.leaf.nextPath,
+      }),
+      allChecksMet,
+      update: {
+        lastOccupiedIndex: nextFreeIndex,
+      },
+    };
+  }
+
+  function computeRoot(
+    witness: LinkedMerkleTreeWitness,
+    newPreviousLeaf: LinkedLeafStruct,
+    newCurrentLeaf: LinkedLeafStruct,
+    isUpdate: Bool,
+    root: Field
+  ) {
+    const { leafPrevious, leafCurrent } = witness;
+
+    leafPrevious.merkleWitness
+      .calculateRoot(leafPrevious.leaf.hash())
+      .equals(root)
+      .or(isUpdate)
+      .assertTrue("Previous leaf calculation not matching");
+
+    const root1 = leafPrevious.merkleWitness.calculateRoot(
+      newPreviousLeaf.hash()
+    );
+
+    let intermediateRoot = Provable.if(isUpdate, root, root1);
+
+    // TODO Make this Provable.if more efficient
+    const leafCurrentLeaf = Provable.if(
+      isUpdate,
+      leafCurrent.leaf.hash(),
+      Field(0)
+    );
+    leafCurrent.merkleWitness
+      .calculateRoot(leafCurrentLeaf)
+      .assertEquals(intermediateRoot);
+
+    return leafCurrent.merkleWitness.calculateRoot(newCurrentLeaf.hash());
+  }
+
+  export function applyTreeWrite(
+    state: LinkedMerkleTreeGlobalState,
+    witness: LinkedMerkleTreeWitness,
+    treeWrite: TreeWrite,
+    index: number
+  ): LinkedMerkleTreeGlobalState {
+    const { leafPrevious, leafCurrent } = witness;
+
+    const isUpdate = leafPrevious.leaf.isDummy();
+
+    // For read-only and update
+    const updateState = update(state, witness, treeWrite);
+
+    // For insert
+    const insertState = insert(state, witness, treeWrite);
+
+    const instruction = chooseInstruction(isUpdate, updateState, insertState);
+
+    instruction.allChecksMet.assertTrue(
+      `Not all witness checks have been met: ${index}`
+    );
+
+    const newRoot = computeRoot(
+      witness,
+      instruction.newPreviousLeaf,
+      instruction.newCurrentLeaf,
+      isUpdate,
+      state.root
+    );
+
+    return {
+      root: newRoot,
+      lastOccupiedIndex: instruction.update.lastOccupiedIndex,
+    };
+  }
+}
+
+/* eslint-enable no-inner-declarations */
---
+++ packages/common/test/trees/LinkedMerkleTreeCircuitOps.test.ts
@@ -0,0 +1,145 @@
+import {
+  InMemoryLinkedLeafStore,
+  InMemoryMerkleTreeStorage,
+  LinkedLeafStruct,
+  LinkedMerkleTree,
+  LinkedMerkleTreeCircuitOps,
+  LinkedMerkleTreeGlobalState,
+  LinkedMerkleTreeWitness,
+} from "../../src";
+import { Field, Provable } from "o1js";
+
+describe("LinkedMerkleTree - Circuit Ops", () => {
+  function setupTree() {
+    const leafStore = new InMemoryLinkedLeafStore();
+    const store = new InMemoryMerkleTreeStorage();
+    return new LinkedMerkleTree(store, leafStore);
+  }
+
+  let tree: LinkedMerkleTree;
+
+  beforeEach(() => {
+    tree = setupTree();
+  });
+
+  it("should correctly verify insert witness", () => {
+    try {
+      const root = tree.getGlobalState();
+      const insertWitness = tree.setLeaf(5n, 1000n);
+
+      const globalState = LinkedMerkleTreeCircuitOps.applyTreeWrite(
+        root,
+        insertWitness,
+        {
+          path: Field(5),
+          from: Field(0),
+          to: Field(1000),
+        },
+        0
+      );
+
+      expect(globalState.root.toString()).toStrictEqual(
+        tree.getRoot().toString()
+      );
+      expect(globalState.lastOccupiedIndex.toString()).toStrictEqual(
+        root.lastOccupiedIndex.add(1).toString()
+      );
+    } catch (e) {
+      console.error(e);
+      throw e;
+    }
+  });
+
+  it("should correctly verify update witness", () => {
+    try {
+      tree.setLeaf(5n, 1000n);
+      tree.setLeaf(10n, 1500n);
+
+      const root = tree.getGlobalState();
+
+      const updateWitness = tree.setLeaf(10n, 500n);
+
+      const globalState = LinkedMerkleTreeCircuitOps.applyTreeWrite(
+        root,
+        updateWitness,
+        {
+          path: Field(10),
+          from: Field(1500),
+          to: Field(500),
+        },
+        0
+      );
+
+      expect(globalState.root.toString()).toStrictEqual(
+        tree.getRoot().toString()
+      );
+      expect(globalState.lastOccupiedIndex.toString()).toStrictEqual(
+        root.lastOccupiedIndex.toString()
+      );
+    } catch (e) {
+      console.error(e);
+      throw e;
+    }
+  });
+
+  it("should not update root when only reading", () => {
+    tree.setLeaf(5n, 1000n);
+    tree.setLeaf(10n, 1500n);
+
+    const root = tree.getGlobalState();
+
+    const updateWitness = tree.getReadWitness(10n);
+
+    const globalState = LinkedMerkleTreeCircuitOps.applyTreeWrite(
+      root,
+      LinkedMerkleTreeWitness.fromReadWitness(updateWitness),
+      {
+        path: Field(10),
+        from: Field(1500),
+        to: Field(1500),
+      },
+      0
+    );
+
+    expect(globalState.root.toString()).toStrictEqual(root.root.toString());
+    expect(globalState.root.toString()).toStrictEqual(
+      tree.getRoot().toString()
+    );
+    expect(globalState.lastOccupiedIndex.toString()).toStrictEqual(
+      root.lastOccupiedIndex.toString()
+    );
+  });
+
+  it("Circuit size", async () => {
+    const root = tree.getGlobalState();
+
+    const updateWitness = tree.setLeaf(10n, 500n);
+
+    const cs = await Provable.constraintSystem(() => {
+      const rootWitness = Provable.witness(
+        LinkedMerkleTreeGlobalState,
+        () => root
+      );
+      const updateWitnessWitness = Provable.witness(
+        LinkedMerkleTreeWitness,
+        () => updateWitness
+      );
+      const treeWrite = {
+        path: Provable.witness(Field, () => 1),
+        from: Provable.witness(Field, () => 1),
+        to: Provable.witness(Field, () => 1),
+      };
+
+      const globalState = LinkedMerkleTreeCircuitOps.applyTreeWrite(
+        rootWitness,
+        updateWitnessWitness,
+        treeWrite,
+        0
+      );
+    });
+
+    console.log(cs.rows);
+
+    // expect(cs.rows).toBeLessThan(2500);
+  });
+});
