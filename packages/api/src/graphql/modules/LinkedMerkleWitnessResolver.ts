0356520ec2c1252735f1991c2b907cdf9d4ef47d
Raphael Panic <raphael.panic@gmx.at>
2025-04-11 15:06:12 +0200

    Adapted api to new lmt types

--- packages/api/src/graphql/modules/LinkedMerkleWitnessResolver.ts
+++ packages/api/src/graphql/modules/LinkedMerkleWitnessResolver.ts
@@ -1,11 +1,13 @@
 import { Arg, Field, ObjectType, Query } from "type-graphql";
 import { inject } from "tsyringe";
 import {
-  LinkedLeafAndMerkleWitness,
   LinkedMerkleTree,
+  LinkedMerkleTreeReadWitness,
 } from "@proto-kit/common";
-import { CachedLinkedMerkleTreeStore } from "@proto-kit/sequencer/dist/state/merkle/CachedLinkedMerkleTreeStore";
-import { AsyncLinkedMerkleTreeStore } from "@proto-kit/sequencer/dist/state/async/AsyncLinkedMerkleTreeStore";
+import {
+  AsyncLinkedLeafStore,
+  CachedLinkedLeafStore,
+} from "@proto-kit/sequencer";
 
 import { GraphqlModule, graphqlModule } from "../GraphqlModule";
 
@@ -14,7 +16,7 @@ import { LeafDTO } from "./LeafResolver";
 
 @ObjectType()
 export class LinkedMerkleWitnessDTO {
-  public static fromServiceLayerObject(witness: LinkedLeafAndMerkleWitness) {
+  public static fromServiceLayerObject(witness: LinkedMerkleTreeReadWitness) {
     const { leaf, merkleWitness } = witness;
     const leafDTO = LeafDTO.fromServiceLayerModel(leaf);
     const witnessDTO = MerkleWitnessDTO.fromServiceLayerObject(merkleWitness);
@@ -40,7 +42,7 @@ export class LinkedMerkleWitnessDTO {
 export class LinkedMerkleWitnessResolver extends GraphqlModule<object> {
   public constructor(
     @inject("AsyncMerkleStore")
-    private readonly treeStore: AsyncLinkedMerkleTreeStore
+    private readonly treeStore: AsyncLinkedLeafStore
   ) {
     super();
   }
@@ -50,12 +52,12 @@ export class LinkedMerkleWitnessResolver extends GraphqlModule<object> {
       "Allows retrieval of merkle witnesses corresponding to a specific path in the appchain's state tree. These proves are generally retrieved from the current 'proven' state",
   })
   public async witness(@Arg("path") path: string) {
-    const syncStore = await CachedLinkedMerkleTreeStore.new(this.treeStore);
+    const syncStore = await CachedLinkedLeafStore.new(this.treeStore);
 
-    const tree = new LinkedMerkleTree(syncStore);
+    const tree = new LinkedMerkleTree(syncStore.treeStore, syncStore);
     await syncStore.preloadKey(BigInt(path));
 
-    const witness = tree.getWitness(BigInt(path));
+    const witness = tree.getReadWitness(BigInt(path));
 
     return LinkedMerkleWitnessDTO.fromServiceLayerObject(witness);
   }
