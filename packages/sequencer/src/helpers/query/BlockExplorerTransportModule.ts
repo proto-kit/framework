e5bae796cd0bf58903a48299a99991df9f41589f
saitunc <saitunc47@gmail.com>
2025-11-12 12:40:33 +0300

    refactor: add return type to getBlock in BlockExplorerTransportModule

--- packages/sequencer/src/helpers/query/BlockExplorerTransportModule.ts
+++ packages/sequencer/src/helpers/query/BlockExplorerTransportModule.ts
@@ -1,6 +1,6 @@
-import { InclusionStatus } from "@proto-kit/api";
+import { BlockModel } from "@proto-kit/api";
 
 export interface BlockExplorerTransportModule {
   waitTxInclusion(txHash: string): Promise<string>;
-  getBlock(param?: string | number): Promise<any>;
+  getBlock(param?: string | number): Promise<BlockModel | undefined>;
 }
