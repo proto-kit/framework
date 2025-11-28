0c3a43c974fdee38cb48e5c43338ab18490af0b5
saitunc <saitunc47@gmail.com>
2025-11-28 10:47:24 +0300

    feat: change MinaSigner config type and signing logics

--- packages/sequencer/src/settlement/MinaSigner.ts
+++ packages/sequencer/src/settlement/MinaSigner.ts
@@ -1,89 +1,135 @@
 import { Field, PrivateKey, PublicKey, Signature, Transaction } from "o1js";
+import { noop } from "@proto-kit/common";
 import { injectable } from "tsyringe";
-import { AppChainModule } from "../appChain/AppChainModule";
 
+export interface SignTxOptions {
+  pubKeys?: PublicKey[];
+  additionalKeys?: PrivateKey[];
+  signWithContract?: boolean;
+}
+
+import { SequencerModule } from "../sequencer/builder/SequencerModule";
 export interface MinaSigner {
-  getContractKeys(): PublicKey[];
-  
-  sign(signatureData: Field[]): Promise<Signature>;
-  
-  signTransaction(  
-    tx: Transaction<false, false>
-  ): Promise<Transaction<false, true>>;
+  getSignerAddress(): PublicKey;
+
+  getContractAddresses(): PublicKey[];
+
+  sign(signatureData: Field[]): Signature;
+  signMessageWithContract(signatureData: Field[]): Signature,
+
+  signTx(
+    tx: Transaction<false, false>,
+    options?: SignTxOptions
+  ): Transaction<false, true>;
+
 }
 
 export interface InMemorySignerConfig {
-  signer: PrivateKey;         
-  contractKeys?: {
-    settlementKey: PrivateKey,
-    dispatchKey: PrivateKey,
-    minaBridgeKey: PrivateKey,
-  }
-  // optional way: 
-  // additionalKeys?: PrivateKey[]
+  signer: PrivateKey;
+  contractKeys: PrivateKey[]; 
+  tokenControllers?: PrivateKey[]; 
+  tokenBridgeKeys?: PrivateKey[];
 }
 
 @injectable()
-export class SettlementSigner
-  extends AppChainModule<InMemorySignerConfig> 
+export class InMemoryMinaSigner
+  extends SequencerModule<InMemorySignerConfig>
   implements MinaSigner
 {
+
+  private _keyMap!: Map<string, PrivateKey>;
+  
   public constructor() {
     super();
   }
 
+  private initializeKeyMap(): void {
+    if (this._keyMap) return; 
+    
+    this._keyMap = new Map();
+
+    this.config.tokenBridgeKeys?.forEach(key => {
+      this._keyMap.set(key.toPublicKey().toBase58(), key);
+    });
+
+    this.config.contractKeys.forEach(key => {
+      this._keyMap.set(key.toPublicKey().toBase58(), key);
+    });
+
+    this.config.tokenControllers?.forEach(key => {
+      this._keyMap.set(key.toPublicKey().toBase58(), key);
+    });
+  }
+
+  public getSignerAddress(): PublicKey{
+    return this.config.signer.toPublicKey();
+  }
+  
   /**
-   * 
    * Contracts public key getter. Since we expect private keys to be managed here, public keys can be returned from this module.
+   * With index order, returned public keys are public keys of: 
+   * [0] -> settlement contract
+   * [1] -> dispatch contract
+   * [2] -> minaBridge contract
    * 
-   * @returns Array of public keys, not ordered yet
-   * 
+   * @returns Array of public keys. 
    */
-  public getContractKeys(): PublicKey[] {
-    const contractKeysObject = this.config.contractKeys ?? {};
+  public getContractAddresses(): PublicKey[] {
+    if(!this.config.contractKeys){
+      return [];
+    }
     
-    // Use Object.values to get the PrivateKey objects and filter out any undefined values.
-    const contractPrivateKeys: PrivateKey[] = Object.values(contractKeysObject).filter(
-      (key): key is PrivateKey => key !== undefined
-    );
+    const contractPrivateKeys = this.config.contractKeys;
     
-    return contractPrivateKeys.map(key => key.toPublicKey());
+    return contractPrivateKeys.map((key) => key.toPublicKey());
   }
 
   /**
    * @param signatureData Data to be signed.
    * @returns Signature signed by signer.
    */
-  public async sign(signatureData: Field[]): Promise<Signature> {
+  public sign(signatureData: Field[]): Signature {
     return Signature.create(this.config.signer, signatureData);
   }
 
-
-  /**
-   * 
-   * Signs the transaction with all available keys (will/should be change and signing must be done in a selective way).
-   * 
-   * @param tx Transaction to be signed with.
-   * @returns signed transaction object
+    /**
+   * @param signatureData Data to be signed.
+   * @returns Signature signed with private key of settlement contract address.
    */
-  public async signTransaction(
-    tx: Transaction<false, false>
-  ): Promise<Transaction<false, true>> {
-    const contractKeysObject = this.config.contractKeys ?? {};
-    
-    // Extract the PrivateKey objects from the configuration object
-    const contractPrivateKeys: PrivateKey[] = Object.values(contractKeysObject).filter(
-      (key): key is PrivateKey => key !== undefined
-    );
-
-    // Combine the primary signer (feepayer) with the contract keys
-    const allKeys: PrivateKey[] = [
-      this.config.signer,
-      ...contractPrivateKeys
-    ];
-
-    // Sign the transaction with the collected keys
-    return tx.sign(allKeys);
+  public signMessageWithContract(signatureData: Field[]): Signature{
+    return Signature.create(this.config.contractKeys[0],signatureData);
   }
 
+  public signTx(
+  tx: Transaction<false, false>,
+  options: SignTxOptions = {}
+): Transaction<false, true> {
+  const { 
+    pubKeys = [], 
+    additionalKeys = [], 
+    signWithContract = false 
+  } = options;
+
+  const privateKeys: PrivateKey[] = [];
+
+  for (const pubKey of pubKeys) {
+    const privKey = this._keyMap.get(pubKey.toBase58());
+    if (!privKey) {
+      throw new Error(`Error in signTx function: relevant private key to ${pubKey.toBase58()} not found!`);
+    }
+    privateKeys.push(privKey);
+  }
+  
+  if (signWithContract) {
+    privateKeys.push(...this.config.contractKeys);
+  }
+  
+  const keys = [this.config.signer, ...privateKeys, ...additionalKeys];
+  return tx.sign(keys);
+}
+
+  public async start() {
+    this.initializeKeyMap();
+    noop();
+  }
 }
