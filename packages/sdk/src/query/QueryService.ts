09f423f86d27924e187ce6cb6367716e5eb6a4ea
saitunc <aytunc.sancar@dynastystudios.io>
2025-12-19 15:03:55 +0300

    refactor: remove unnecessary default parameter

--- packages/sdk/src/query/QueryService.ts
+++ packages/sdk/src/query/QueryService.ts
@@ -25,8 +25,7 @@ import {
 export class QueryService<
   RuntimeModules extends RuntimeModulesRecord,
   ProtocolModules extends ProtocolModulesRecord &
-    MandatoryProtocolModulesRecord = ProtocolModulesRecord &
-    MandatoryProtocolModulesRecord,
+    MandatoryProtocolModulesRecord
 > {
   // Lazily initialized query instances
   private RuntimeQuery?: Query<RuntimeModule<unknown>, RuntimeModules>;
