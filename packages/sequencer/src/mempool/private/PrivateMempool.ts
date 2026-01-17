178c16367fd23bd550c7a2f72a01cd82e1b00052
Raphael Panic <raphael.panic@gmx.at>
2026-01-17 10:26:18 +0100

    Fixed compile error

--- packages/sequencer/src/mempool/private/PrivateMempool.ts
+++ packages/sequencer/src/mempool/private/PrivateMempool.ts
@@ -66,11 +66,11 @@ export class PrivateMempool extends SequencerModule implements Mempool {
 
   @trace("mempool.get_txs")
   public async getTxs(
-    offset: number,
+    offset?: number,
     limit?: number
   ): Promise<PendingTransaction[]> {
     return await this.transactionStorage.getPendingUserTransactions(
-      offset,
+      offset ?? 0,
       limit
     );
   }
