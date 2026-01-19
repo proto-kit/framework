cb059f470edaf8dfc8b324fc0b163508fe20d04c
saitunc <aytunc.sancar@dynastystudios.io>
2026-01-19 23:30:46 +0300

    fix(test): fix test code to pass

--- packages/module/test/runtimeMethod.test.ts
+++ packages/module/test/runtimeMethod.test.ts
@@ -91,10 +91,10 @@ describe("runtimeMethod", () => {
     expect.assertions(1 + parameters.length);
 
     const module = runtime.resolve("Balances");
-
+    const parametersArray = parameters.flatMap((x) => x.toFields().map(f => f.toString()));
     const decoder = MethodParameterEncoder.fromMethod(module, "getBalance");
     const recodedParameters = await decoder.decode(
-      parameters.flatMap((x) => x.toFields()),
+      parametersArray,
       []
     );
 
