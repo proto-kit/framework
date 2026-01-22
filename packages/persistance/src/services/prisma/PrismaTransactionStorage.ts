c66227033e766485fd9261e73afd673e8d7bc3c7
Raphael Panic <raphael.panic@gmx.at>
2026-01-22 22:06:54 +0100

    Implemented input paths for prisma

--- packages/persistance/src/services/prisma/PrismaTransactionStorage.ts
+++ packages/persistance/src/services/prisma/PrismaTransactionStorage.ts
@@ -9,6 +9,7 @@ import {
 import type { PrismaConnection } from "../../PrismaDatabaseConnection";
 
 import { TransactionMapper } from "./mappers/TransactionMapper";
+import { Decimal } from "./PrismaStateService";
 
 @injectable()
 export class PrismaTransactionStorage implements TransactionStorage {
@@ -126,5 +127,30 @@ export class PrismaTransactionStorage implements TransactionStorage {
       block,
       batch,
     };
+  }
+
+  public async reportSkippedTransactions(
+    paths: Record<string, bigint[]>
+  ): Promise<void> {
+    const { prismaClient } = this.connection;
+
+    await prismaClient.skippedTransactionInputPaths.createMany({
+      data: Object.entries(paths).map(([transactionHash, pathArray]) => ({
+        transactionHash,
+        paths: pathArray.map((path) => new Decimal(path.toString())),
+      })),
+    });
+  }
+
+  public async reportChangedPaths(paths: bigint[]): Promise<void> {
+    const { prismaClient } = this.connection;
+
+    await prismaClient.skippedTransactionInputPaths.deleteMany({
+      where: {
+        paths: {
+          hasSome: paths.map((path) => new Decimal(path.toString())),
+        },
+      },
+    });
   }
 }
