284d6424f3df478c5fa32fe34bf206f2e1d9a1da
saitunc <aytunc.sancar@dynastystudios.io>
2026-01-22 12:29:59 +0300

    feat: add a decorator method for checking progress of methods

---
+++ packages/sequencer/src/helpers/BusyGuard.ts
@@ -0,0 +1,37 @@
+import { log } from "@proto-kit/common";
+/**
+ * Decorator that ensures a function/method is not currently in use.
+ * Mostly useful for production of blocks, batches and tasks.
+ */
+export function ensureNotBusy() {
+  return function InnerFunction(
+    target: object,
+    methodName: string,
+    descriptor: TypedPropertyDescriptor<(...args: any[]) => Promise<any>>
+  ) {
+    const originalMethod = descriptor.value!;
+
+    descriptor.value = async function decorator(
+      this: { inProgress: boolean },
+      ...args: any[]
+    ) {
+      if (this.inProgress === true) {
+        log.info(`${methodName.toString()} is in use at the moment.`);
+        return undefined;
+      }
+
+      this.inProgress = true;
+      try {
+        return await originalMethod.apply(this, args);
+      } catch (error: unknown) {
+        if (error instanceof Error) {
+          throw error;
+        } else {
+          log.error(error);
+        }
+      } finally {
+        this.inProgress = false;
+      }
+    };
+  };
+}
