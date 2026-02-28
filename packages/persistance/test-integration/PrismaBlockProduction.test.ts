f8a52edb7d90a0a4762e378166fccd6b13003e13
Raphael Panic <raphael.panic@gmx.at>
2026-02-28 16:43:52 -0300

    Fix broken jest typing

--- packages/persistance/test-integration/PrismaBlockProduction.test.ts
+++ packages/persistance/test-integration/PrismaBlockProduction.test.ts
@@ -7,8 +7,8 @@ import { AppChainTransaction } from "@proto-kit/sdk";
 import { Block, Batch } from "@proto-kit/sequencer";
 import { PrivateKey, PublicKey } from "o1js";
 import { container } from "tsyringe";
-import { testBlockProduction } from "@proto-kit/sequencer/test/integration/BlockProduction-test";
 
+import { testBlockProduction } from "../../../packages/sequencer/test/integration/BlockProduction-test";
 import {
   PrismaBatchStore,
   PrismaBlockStorage,
@@ -24,13 +24,17 @@ import {
 
 describe("Prisma block production", () => {
   const { prismaConfig, redisConfig } = IntegrationTestDBConfig;
-  testBlockProduction(PrismaRedisDatabase, {
-    prisma: {
-      connection: prismaConfig,
-      log: [{ level: "query", emit: "event" }],
-    },
-    redis: redisConfig,
-  });
+  // TODO Fix /src and /dist type mismatch
+  testBlockProduction(
+    PrismaRedisDatabase as any,
+    {
+      prisma: {
+        connection: prismaConfig,
+        log: [{ level: "query", emit: "event" }],
+      },
+      redis: redisConfig,
+    } as never
+  );
 });
 
 describe("prisma integration", () => {
